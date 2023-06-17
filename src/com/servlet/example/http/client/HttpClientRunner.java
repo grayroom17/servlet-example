560891b6bfdcabea79bda28d73e2269bda9378c7
Sergey Deev <mygrayroom@gmail.com>
2023-06-17 20:09:27 +0300

    Multi thread http server

--- src/com/servlet/example/http/client/HttpClientRunner.java
+++ src/com/servlet/example/http/client/HttpClientRunner.java
@@ -7,10 +7,12 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class HttpClientRunner {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         HttpClient client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_1_1)
                 .build();
@@ -21,8 +23,14 @@ public class HttpClientRunner {
                 .POST(BodyPublishers.ofFile(Path.of("resources", "example.json")))
                 .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.headers());
-        System.out.println(response.body());
+//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.headers());
+//        System.out.println(response.body());
+
+        CompletableFuture<HttpResponse<String>> response1 = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        CompletableFuture<HttpResponse<String>> response2 = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        CompletableFuture<HttpResponse<String>> response3 = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response3.get().body());
     }
 }
--- src/com/servlet/example/http/server/HttpServerRunner.java
+++ src/com/servlet/example/http/server/HttpServerRunner.java
@@ -7,13 +7,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class HttpServerRunner {
-
+    private final ExecutorService executorService;
     private final int port;
+    private boolean stopped;
 
     public HttpServerRunner(int port) {
         this.port = port;
+        this.executorService = null;
+    }
+
+    public HttpServerRunner(int port, int poolSize) {
+        this.port = port;
+        this.executorService = Executors.newFixedThreadPool(poolSize);
     }
 
     public void run() {
@@ -21,7 +31,23 @@ public class HttpServerRunner {
             Socket socket;
             try (ServerSocket server = new ServerSocket(port)) {
                 socket = server.accept();
+                System.out.println("Socket accepted");
                 processSocket(socket);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void runAsync() {
+        try {
+            Socket socket;
+            try (ServerSocket server = new ServerSocket(port)) {
+                while (!stopped) {
+                    socket = server.accept();
+                    Socket finalSocket = socket;
+                    executorService.submit(() -> processSocket(finalSocket));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -33,6 +59,7 @@ public class HttpServerRunner {
              var inputStream = new DataInputStream(socket.getInputStream());
              var outputStream = new DataOutputStream(socket.getOutputStream())) {
             System.out.println("Request: " + new String(inputStream.readNBytes(400)));
+            TimeUnit.SECONDS.sleep(5);
 
             var body = Files.readAllBytes(Path.of("resources", "example.html"));
             var headers = """
@@ -43,14 +70,18 @@ public class HttpServerRunner {
             outputStream.write(headers.getBytes());
             outputStream.write(System.lineSeparator().getBytes());
             outputStream.write(body);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        var server = new HttpServerRunner(5758);
-        server.run();
+        var server = new HttpServerRunner(5758, 100);
+//        server.run();
+        server.runAsync();
     }
 
+    public void setStopped(boolean stopped) {
+        this.stopped = stopped;
+    }
 }
