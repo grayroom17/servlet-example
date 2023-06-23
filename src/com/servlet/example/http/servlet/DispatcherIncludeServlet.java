d0ee3d9561733a7c41909987d623414e6a2d6665
DeevS <deev@advantum.pro>
2023-06-23 13:09:15 +0300

    Sessions part 3

--- src/com/servlet/example/http/servlet/DispatcherIncludeServlet.java
+++ src/com/servlet/example/http/servlet/DispatcherIncludeServlet.java
@@ -4,33 +4,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
-import com.servlet.example.http.service.FlightService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/flights")
-public class FlightServlet extends HttpServlet {
-
-    private final FlightService flightService = FlightService.getInstance();
+@WebServlet("/dispatcher-include")
+public class DispatcherIncludeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/flights").include(req, resp);
+
         resp.setContentType("text/html");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
         PrintWriter writer = resp.getWriter();
-        writer.write("<h1>Список перелетов</h1>");
-        writer.write("<ul>");
-        flightService.findAll().forEach(dto -> writer.write(
-                """
-                        <li>
-                        <a href="/tickets?flightId=%d">%s</a>
-                        </li>"""
-                        .formatted(dto.getId(), dto.getDescription())
-        ));
-        writer.write("</ul>");
+        writer.write("25");
     }
 }
